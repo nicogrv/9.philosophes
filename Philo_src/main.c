345a84f5912d94d0917989cc02752bf39e012663
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-04-12 11:50:30 +0200

    1 philo & malloc

--- Philo_src/main.c
+++ Philo_src/main.c
@@ -6,7 +6,7 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:18:24 by ngriveau          #+#    #+#             */
-/*   Updated: 2023/04/12 11:30:48 by ngriveau         ###   ########.fr       */
+/*   Updated: 2023/04/12 11:39:03 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,6 @@ void	main_death(t_philo *philo, t_human *human)
 		}
 		human = human->next;
 		usleep(50);
-		// ft_usleep(philo, 5);
 	}
 }
 
